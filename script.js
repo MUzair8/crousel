e420002a7981178a7d5d6b7d6c9995586d73c277
MUzair8 <uzairrafiq878@gmail.com>
2024-07-27 12:33:16 +0500

    solve imgs issues

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 
-let myImg=[ "file:///C:/Users/AL%20HABIB%20TRADE/Downloads/Arnold_Schwarzenegger_1974.jpg","file:///C:/Users/AL%20HABIB%20TRADE/Downloads/b0f05acad8403908627b3657a6e3004f.jpg", "file:///C:/Users/AL%20HABIB%20TRADE/Downloads/arn.jpg","file:///C:/Users/AL%20HABIB%20TRADE/Downloads/arnn.jpg",
-    "file:///C:/Users/AL%20HABIB%20TRADE/Downloads/sddefault.jpg"
+let myImg = ["imgs/arn.jpg", "imgs/arnn.jpg", "imgs/arnold.jpg", "imgs/b0f05acad8403908627b3657a6e3004f.jpg"
 ]
 
 
-let currentIndex=0;
+let currentIndex = 0;
 let totalImg = myImg.length;
 let img = document.querySelector("#img")
 let nextBtn = document.querySelector("#nextBtn")
 let prevBtn = document.querySelector("#prevBtn")
 
-function showImage(){
-img.src = myImg[currentIndex];
+function showImage() {
+    img.src = myImg[currentIndex];
 
 }
-nextBtn.addEventListener("click", function(){
-    currentIndex=(currentIndex + 1 ) %totalImg
+nextBtn.addEventListener("click", function () {
+    currentIndex = (currentIndex + 1) % totalImg
     showImage()
 });
 
-prevBtn.addEventListener("click", function(){
-    currentIndex=(currentIndex - 1 + totalImg ) %totalImg
+prevBtn.addEventListener("click", function () {
+    currentIndex = (currentIndex - 1 + totalImg) % totalImg
     showImage()
 });
 
